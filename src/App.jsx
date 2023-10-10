a7fa3e384a6c805afa7c2240b61865f46e4dd6d0
Pedro <pedrohm92@gmail.com>
2023-10-10 21:02:41 +0200

    Instalado Tailwind y SASS y creados primeros componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
     <>
-      <h1>Página de inicio</h1>
+      <header className="quiz-header">
+        <h1>Página de inicio</h1>
+      </header>
+      <main className="quiz-container">
+        <h2 className="quiz-title">Pregunta para el Quiz</h2>
+        <ul className="quiz-list">
+          <li>Respuesta 1</li>
+          <li>Respuesta 2</li>
+          <li>Respuesta 3</li>
+          <li>Respuesta 4</li>
+        </ul>
+      </main>
     </>
   );
 }
